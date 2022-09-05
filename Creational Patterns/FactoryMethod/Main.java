4512dd1c6605e9ae8abfe71619f5991534d6c99a
duclongdev <20521566@gm.uit.edu.vn>
2022-09-05 21:24:38 +0700

    add factory method

---
+++ Creational Patterns/FactoryMethod/Main.java
@@ -0,0 +1,10 @@
+package FactoryMethod;
+
+import FactoryMethod.student.Student;
+
+class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.show();
+    }
+}
---
+++ Creational Patterns/FactoryMethod/student/Student.java
@@ -0,0 +1,11 @@
+package FactoryMethod.student;
+
+public class Student {
+    public void show() {
+        System.out.println("dit con me");
+    }
+
+    public Student() {
+
+    }
+}
